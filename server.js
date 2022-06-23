6306f78d92cd7fd5bc8f5d9167120ad879c9fd80
minwooKang <rkdalsdn94@gmail.com>
2022-06-23 19:20:24 +0900

    feat: MongoDB 연동 설정 및 env 패키지 추가
    dotenv package 추가
    ignore 파일 등록
    MongoDB 연동
    .env.sample 파일에 몽고 url 등록해야 된다.

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
+require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
 
 const PORT = 5001;
 const app = express();
 const productRoutes = require('./routes'); // 라우팅 처리
+
+mongoose
+  .connect(process.env.mongoURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('MongoDB Connected...'))
+  .catch((err) => console.log(err));
 
 // 미들웨어 등록
 app.use(express.json()); // express 내장 미들웨어 - bodyParser랑 동일함
