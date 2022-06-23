c423d70fc022cf85b18d3a347a13f2d400c42138
minwooKang <rkdalsdn94@gmail.com>
2022-06-23 16:38:56 +0900

    feat: controller 등록
    라우터 경로를 통해 들어온 작업을 미들웨어를 통해 역할 분리

--- routes.js
+++ routes.js
@@ -3,25 +3,8 @@
 
 const express = require('express');
 const router = express.Router();
+const productController = require('./controller/product');
 
-router.get('/', (req, res) => {
-  res.send('안녕하세요');
-});
-
-app.post('/products', (req, res) => {
-  console.log('req.body : ', req.body); // 미들웨어 등록을 해야 여기와 json형식의 파일을 읽을 수 있음
-});
-
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
-
-app.post('/products', (req, res) => {
-  console.log('req.body : ', req.body); // 미들웨어 등록을 해야 여기와 json형식의 파일을 읽을 수 있음
-});
-
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
+router.get('/', productController.hello); // 미들웨어 부분을 따로 별도로 처리하는 부분
 
 module.exports = router;
--- server.js
+++ server.js
@@ -6,7 +6,11 @@ const productRoutes = require('./routes'); // 라우팅 처리
 
 // 미들웨어 등록
 app.use(express.json()); // express 내장 미들웨어 - bodyParser랑 동일함
-app.use('/api/products', productRoutes); // 라우팅 미들웨어 등록
+app.use('/api/products', productRoutes); // '/api/products' 라우팅 미들웨어 등록
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
 
 app.listen(PORT);
 console.log(`Running on port ${PORT}`);
