e9ab7aa8b0ca13bca41780b8292e94573842ac8a
minwooKang <rkdalsdn94@gmail.com>
2022-06-24 14:34:21 +0900

    test: product안에 create 함수의 타입이 function이 맞는지 tdd 방식으로 진행
    1. test/unit/products.test.js 파일에 먼저 테스트 코드를 작성 후
    2. controller/product.js 파일로 이동해서 함수 정의 후
    3. 다시 products.test.js 파일에서 controller 파일에 있는 함수를 테스트 진행

--- controller/product.js
+++ controller/product.js
@@ -1,3 +1,6 @@
 exports.hello = (req, res) => {
   res.send('안녕하세요');
 };
+
+// 2. 테스트 파일에서 만든 함수를 뼈대만 정의한다. -> 테스트 파일로 이동
+exports.createProduct = () => {};
--- test/unit/products.test.js
+++ test/unit/products.test.js
@@ -9,3 +9,13 @@ describe('Calculation', () => {
     expect(2 + 2).not.toBe(5);
   });
 });
+
+// 3. controller/product.js 안에 함수를 불러온다.
+const productController = require('../../controller/product');
+
+// 1. 먼저 테스트 코드를 만든다. -> controller/product.js 파일로 이동
+describe('Product Controller Create', () => {
+  it('should have a createProduct function', () => {
+    expect(typeof productController.createProduct).toBe('function');
+  });
+});
