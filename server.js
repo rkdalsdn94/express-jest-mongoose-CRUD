3eae6abce264fd63ccab5f46d8188e42a46cfa4b
minwooKang <rkdalsdn94@gmail.com>
2022-06-25 23:28:12 +0900

    test: product create 통합 테스트 추가
    product create 통합 테스트 추가
    통합테스트 추가를 위해 server.js에 있는 express app을 export 함

--- server.js
+++ server.js
@@ -24,3 +24,5 @@ app.get('/', (req, res) => {
 
 app.listen(PORT);
 console.log(`Running on port ${PORT}`);
+
+module.exports = app;
--- test/integration/products.int.test.js
+++ test/integration/products.int.test.js
@@ -1,1 +1,12 @@
 // 통합 테스트를 위한 공간.
+const request = require('supertest');
+const app = require('../../server');
+const newProduct = require('../data/new-product.json');
+
+it('POST /api/products', async () => {
+  const response = await request(app).post('/api/products').send(newProduct);
+
+  expect(response.statusCode).toBe(201);
+  expect(response.body.name).toBe(newProduct.name);
+  expect(response.body.description).toBe(newProduct.description);
+});
